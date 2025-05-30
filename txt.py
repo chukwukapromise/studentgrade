260a18e01a63fbebab673b52b592e773b877044a
chukwukapromise <126197425+chukwukapromise@users.noreply.github.com>
2025-05-30 12:44:23 +0100

    Delete txt.py

--- txt.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-driver = webdriver.Chrome()
-driver.get("https://www.jumia.com.ng/")
-print("Successfully opened Jumia website.")
-time.sleep(5)
